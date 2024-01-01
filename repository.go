44f81a516662e31264b98399afd8072be8b9423d
yang,zhong <yang-zhong@udious.com>
2024-01-01 10:08:58 +0800

    ccc

--- repository.go
+++ repository.go
@@ -2,11 +2,8 @@ package repository
 
 import (
 	"context"
-	"crypto/md5"
-	"encoding/hex"
 	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"gorm.io/gorm"
@@ -25,8 +22,8 @@ const (
 	AfterUpdate
 	BeforeDelete
 	AfterDelete
-	BeforeSave
-	AfterSave
+	BeforeUpdateFields
+	AfterUpdateFields
 )
 
 const (
@@ -146,130 +143,14 @@ func Count(field string) string {
 	return fmt.Sprintf("COUNT(%s)", field)
 }
 
-type MatchOptions struct {
-	Matches []MatchItem
-	Sort    []string
-	Limit   *int
-	Offset  *int
-}
-
-func (opts MatchOptions) Sum() string {
-	str := ""
-	for _, m := range opts.Matches {
-		str += fmt.Sprintf("%s%d%v", m.Field, m.Operator, m.Value)
-	}
-	sum := md5.Sum([]byte(fmt.Sprintf("%s%s%d%d", str, strings.Join(opts.Sort, ""), func() int {
-		if opts.Limit != nil {
-			return *opts.Limit
-		}
-		return 0
-	}(), func() int {
-		if opts.Offset != nil {
-			return *opts.Offset
-		}
-		return 0
-	}())))
-	return hex.EncodeToString(sum[:])
-}
-
-type MatchOption func(*MatchOptions)
-
-func (opts *MatchOptions) oper(field string, oper Operator, val interface{}) *MatchOptions {
-	opts.Matches = append(opts.Matches, MatchItem{
-		Field: field, Operator: oper, Value: val,
-	})
-	return opts
-}
-
-func (opts *MatchOptions) Apply(newOptions ...MatchOption) MatchOptions {
-	for _, n := range newOptions {
-		n(opts)
-	}
-	return *opts
-}
-
-func (opts *MatchOptions) OR(options ...MatchOption) *MatchOptions {
-	var o MatchOptions
-	o.Apply(options...)
-	opts.Matches = append(opts.Matches, MatchItem{Operator: OR, Value: o})
-	return opts
-}
-
-func (opts *MatchOptions) Quote(options ...MatchOption) {
-	var o MatchOptions
-	o.Apply(options...)
-	opts.Matches = append(opts.Matches, MatchItem{Operator: Quote, Value: o})
-}
-
-func (opts *MatchOptions) AND(sub ...MatchOption) *MatchOptions {
-	var o MatchOptions
-	o.Apply(sub...)
-	opts.Matches = append(opts.Matches, MatchItem{Operator: AND, Value: o})
-	return opts
-}
-
-func (opts *MatchOptions) EQ(field string, val any) *MatchOptions {
-	return opts.oper(field, EQ, val)
-}
-
-func (opts *MatchOptions) NEQ(field string, val any) *MatchOptions {
-	return opts.oper(field, NEQ, val)
-}
-
-func (opts *MatchOptions) LT(field string, val any) *MatchOptions {
-	return opts.oper(field, LT, val)
-}
-
-func (opts *MatchOptions) LTE(field string, val any) *MatchOptions {
-	return opts.oper(field, LTE, val)
-}
-
-func (opts *MatchOptions) GT(field string, val any) *MatchOptions {
-	return opts.oper(field, GT, val)
-}
-
-func (opts *MatchOptions) GTE(field string, val any) *MatchOptions {
-	return opts.oper(field, GTE, val)
-}
-
-func (opts *MatchOptions) LIKE(field string, val any) *MatchOptions {
-	return opts.oper(field, LIKE, val)
-}
-
-func (opts *MatchOptions) IN(field string, val any) *MatchOptions {
-	return opts.oper(field, IN, val)
-}
-
-func (opts *MatchOptions) NotIN(field string, val any) *MatchOptions {
-	return opts.oper(field, NOTIN, val)
-}
-
-func (opts *MatchOptions) Null(field string) *MatchOptions {
-	return opts.oper(field, NULL, nil)
-}
-
-func (opts *MatchOptions) NotNull(field string) *MatchOptions {
-	return opts.oper(field, NOTNULL, nil)
-}
-
-func (opts *MatchOptions) SetLimit(limit int) *MatchOptions {
-	opts.Limit = &limit
-	return opts
-}
-
-func (opts *MatchOptions) SetOffset(offset int) *MatchOptions {
-	opts.Offset = &offset
-	return opts
-}
-
-func (opts *MatchOptions) SetSort(sort ...string) *MatchOptions {
-	opts.Sort = sort
-	return opts
-}
-
 type DeletedAt struct {
 	Id        string     `json:"id" gorm:"column:id"`
 	DeletedAt *time.Time `json:"deleted_at" gorm:"column:deleted_at"`
+}
+
+type ChangeData struct {
+	Model string
+	Data  any
 }
 
 type Repository interface {
@@ -288,11 +169,5 @@ type Repository interface {
 	// UpdateField field
 	UpdateFields(ctx context.Context, fields Fields, opts ...MatchOption) error
 	// Hook
-	Hook(oper int, callback func(tx *gorm.DB) error)
-}
-
-func Sum(opts ...MatchOption) string {
-	var opt MatchOptions
-	opt.Apply(opts...)
-	return opt.Sum()
+	Hook(oper int, callback func(m ChangeData, tx *gorm.DB) error)
 }
--- sqlite_test.go
+++ sqlite_test.go
@@ -24,4 +24,7 @@ func TestSqlite_test(t *testing.T) {
 		M(&bookWithUser, &Book{}).With(&User{}, AuthorID(Field("users.id"))),
 		AuthorID([]string{"1", "2", "3"}),
 	)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
